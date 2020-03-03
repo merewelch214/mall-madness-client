3103b6232b6b29c8ecc78538d58553a5574e06a6
merewelch214 <merewelch@gmail.com>
2020-03-03 11:34:40 -0500

    more cart stuff

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,37 +3,25 @@ import CartButtons from './CartButtons'
 import EditButtons from './EditButtons'
 
 class Product extends React.Component {
-
-    getRightButtons = () => {
-        console.log('hitting func')
-        if (this.props.currentUser.role === 'shopper' && (!this.props.cart_id || this.props.cart_id === this.props.currentUser.cart.id)) {
-            return <CartButtons currentUser={this.props.currentUser} currentProductId={this.props.id}/>
-        } else if (this.props.currentUser.role === 'shopper' && this.props.cart_id) {
-            return <p>This product is out of stock.</p>
-        } else if (this.props.currentUser.role === 'owner' && !this.props.cart_id) {
-            return <EditButtons currentUser={this.props.currentUser} currentProductId={this.props.id}/>
-        } else if (this.props.currentUser.role === 'owner' && this.props.cart_id) {
-            return <p>This product has sold!</p>
-        }
-    }
     
     render(){
-        console.log(this.getRightButtons)
+
         const userType = this.props.currentUser.role
         const cartId = this.props.cart_id
         let buttons;
 
-        if (userType === 'shopper' && (!cartId || cartId === this.props.currentUser.cart.id)) {
-            buttons = <CartButtons currentUser={this.props.currentUser} currentProductId={this.props.id}/>
+
+        if ((cartId === this.props.currentUser.cart.id || cartId === null) && userType === 'shopper') {
+            buttons = <CartButtons currentProduct={this.props}/>
         } else if (userType === 'shopper' && cartId) {
             buttons = <p>This product is out of stock.</p>
-        } else if (userType === 'owner' && !cartId) {
-            buttons = <EditButtons currentUser={this.props.currentUser} currentProductId={this.props.id}/>
+        } else if (userType === 'owner' && cartId === null) {
+            buttons = <EditButtons currentProduct={this.props}/>
         } else if (userType === 'owner' && cartId) {
             buttons = <p>This product has sold!</p>
-        }
+        } 
 
-        return (
+        return (    
         <div>
             <h1>{this.props.name}</h1>
             <img src={this.props.img_url} alt={this.props.name}/>
@@ -47,5 +35,7 @@ export default Product;
 
 // if the user is an owner and product has not sold, show them edit / delete buttons
 // if the user is an owner and the product has sold, do not show any buttons
-// if the user is a shopper and the product does not have a cart id, show them an add button
-// if the user is a shopper and the produt has a cart it, show them the product is out of stock
+// if the user is a shopper and the product does not have a cart id, show them an add to cart button
+// if the user is a shopper and the product has a cart id, but it is not their cart, show them the product is out of stock
+// if the user is a shopper and the product is in their cart, show them a message that it's intheir cart
+// in the Cart component, add a remove from cart button
--- src/containers/Cart.js
+++ src/containers/Cart.js
@@ -13,14 +13,19 @@ class Cart extends React.Component {
     }
     
     render(){
-            return (
+        console.log('state:', this.state )
+        console.log('props', this.props)
+        const { products } = this.state
+  
+        return (
             <div>
                 <h1>Cart</h1>
-                {this.state.products.length > 1 ? 'Items in your cart' : 'Your cart is empty'}
+                {products ? products.map(product => {
+                    return <Product key={product.id} currentUser={this.props.currentUser} {...product} />
+                }) : 'Your cart is empty.'}
             </div>
         )
     }
 }
 
 export default Cart;
-// this.products.map(product => product.name)
